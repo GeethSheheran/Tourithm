a0d74426ef6abc78034fd176ecc7af4ed7589c60
geethsheheran <68048419+GeethSheheran@users.noreply.github.com>
2025-05-15 22:12:23 +0530

    changes

--- ruizarch/src/app/_components/sections/FeaturesTwo.jsx
+++ ruizarch/src/app/_components/sections/FeaturesTwo.jsx
@@ -5,7 +5,7 @@ const FeaturesTwoSection = () => {
     <>
         {/* features two */}
         <section className="mil-soft-bg mil-relative">
-            <img src="/img/other/bg.svg" className="mil-bg-img" alt="image" />
+            <img src="/img/photo/bg.svg" className="mil-bg-img" alt="image" />
 
             <div className="container mil-p-120-60">
                 <div className="mil-background-grid mil-softened" />
