89d3e1442b868d2a33293bb6a1e5cd10043b5476
geethsheheran <68048419+GeethSheheran@users.noreply.github.com>
2025-05-29 23:45:50 +0530

    Update page.jsx

--- ruizarch/src/app/(pages)/search/page.jsx
+++ ruizarch/src/app/(pages)/search/page.jsx
@@ -1,50 +1,50 @@
-import Filteredblog from '@/src/app/_components/Filteredblog';
+// import Filteredblog from '@/src/app/_components/Filteredblog';
 
-import AppData from "@data/app.json";
+// import AppData from "@data/app.json";
 
-import PageBanner from "@components/PageBanner";
-import Sidebar from "@components/Sidebar";
+// import PageBanner from "@components/PageBanner";
+// import Sidebar from "@components/Sidebar";
 
-import { generateJsonPostsData } from "@library/posts";
+// import { generateJsonPostsData } from "@library/posts";
 
-export const metadata = {
-  title: {
-		default: "Search",
-	},
-  description: AppData.settings.siteDescription,
-}
+// export const metadata = {
+//   title: {
+// 		default: "Search",
+// 	},
+//   description: AppData.settings.siteDescription,
+// }
 
-import { promises as fs } from 'fs';
+// import { promises as fs } from 'fs';
 
-async function Search() {
-  const generateJsonPosts = await generateJsonPostsData();
-  const file = await fs.readFile(process.cwd() + '/src/data/.json/posts.json', 'utf8');
-  const posts = JSON.parse(file);
+// async function Search() {
+//   const generateJsonPosts = await generateJsonPostsData();
+//   const file = await fs.readFile(process.cwd() + '/src/data/.json/posts.json', 'utf8');
+//   const posts = JSON.parse(file);
 
-  return (
-    <>
-      <PageBanner pageTitle={"Search: %s"} breadTitle={"Search"} bgImage={"/img/photo/12.jpg"} />
+//   return (
+//     <>
+//       <PageBanner pageTitle={"Search: %s"} breadTitle={"Search"} bgImage={"/img/photo/12.jpg"} />
 
-      {/* blog */}
-      <section>
-          <div className="container mil-p-120-60">
-              <div className="mil-background-grid mil-softened"></div>
-              <div className="row justify-content-between">
-                  <div className="col-lg-7">
+//       {/* blog */}
+//       <section>
+//           <div className="container mil-p-120-60">
+//               <div className="mil-background-grid mil-softened"></div>
+//               <div className="row justify-content-between">
+//                   <div className="col-lg-7">
                       
-                      <Filteredblog posts={posts} />
+//                       <Filteredblog posts={posts} />
 
-                  </div>
-                  <div className="col-lg-5">
+//                   </div>
+//                   <div className="col-lg-5">
 
-                      <Sidebar />
+//                       <Sidebar />
 
-                  </div>
-              </div>
-          </div>
-      </section>
-      {/* blog end */}
-    </>
-  );
-};
-export default Search;
+//                   </div>
+//               </div>
+//           </div>
+//       </section>
+//       {/* blog end */}
+//     </>
+//   );
+// };
+// export default Search;
